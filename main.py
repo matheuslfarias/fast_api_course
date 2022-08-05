7e0d053fa70cb2aa321fdf8c69447ae97700d289
Matheus <matheusmlf@gmail.com>
2022-08-05 07:58:33 -0300

    Criando os primeiros endpoints - GET e GET{id}

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
+from fastapi import HTTPException
+from fastapi import status
+
 
 app = FastAPI()
+
+
+# Dicionário de cursos, aqui serve como banco de dados.
 
 cursos = {
     1: {
@@ -15,6 +21,18 @@ cursos = {
     }   
 }
 
+# Criando primeiro  - GET
+@app.get('/cursos')
+async def get_cursos():
+    return cursos
+
+# GET - ID
+@app.get('/cursos/{id}')
+async def get_curso(id: int): 
+    curso = cursos.get(id)
+    curso.update({"id": id})
+    return curso
+
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000,debug=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000,debug=True,reload=True)
