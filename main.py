7349fc63bc07ba1604e85293d177b2c71f32e858
Matheus <matheusmlf@gmail.com>
2022-08-05 08:08:12 -0300

    Updated - GET ID

--- main.py
+++ main.py
@@ -28,11 +28,14 @@ async def get_cursos():
 
 # GET - ID
 @app.get('/cursos/{id}')
-async def get_curso(id: int): 
-    curso = cursos.get(id)
-    curso.update({"id": id})
-    return curso
-
+async def get_curso(id: int):
+    try:
+        return cursos[id]
+    except KeyError:  # Trata o erro de id não encontrado.
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Curso não encontrado")
+    
+    
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", host="0.0.0.0", port=8000,debug=True,reload=True)
